cb44129b94d0ca14c5a4eaa0e5fa6cf836e15391
yuanrui <yuanrui@scnee.com>
2015-04-22 09:59:39 +0800

    Name:YR Time:2015.04.22 10:00:00 Changed:main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,4 +7,6 @@ void main( int argc, char *argv[])
 	cout<<"hello world, I'm guwb-win7"<<endl;
 
 	cout<<"Be the change you wanna see in the world"<<endl;
+
+	cout<<"helloguwb-win7, I'm YR-win7,Nice to meet you!"<<endl;
 }
